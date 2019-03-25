be2a9d1fea33dc334c9d87ef4d6a938569d9e754
Altusya <goldmoon7@email.cz>
2019-03-25 19:11:41 +0100

    DU zacatek

--- domaci-ukol.sql
+++ domaci-ukol.sql
@@ -2,13 +2,24 @@
 # 1. Vypište všechny učitele kteří neučí žádný předmět
 
 SELECT t.name AS učitele_kteří_neučí_žádný_předmět
+
 FROM teacher t
+
     LEFT JOIN class_subject cs ON t.id = cs.teacher_id
+
     WHERE cs.teacher_id IS NULL;
 
 #####################
 # 2. Vypište všechny žáky, kteří mají alespoň jednu 5 a zároveň vypište kolik jich mají
 
-SELECT pupil_id AS zaky5
+SELECT p.name AS žáky,
 
-FROM pupil_subject ps WHERE ps.grade = 5
+COUNT(*) AS pocet_petek
+
+FROM pupil_subject ps
+
+    JOIN pupil p ON ps.pupil_id = p.id
+
+WHERE ps.grade = 5
+
+GROUP BY p.name
